6fcfeb5a1d436a30d4d22de816157f7d5a74fe76
Luiz Almeida <luizhrqas@gmail.com>
2022-01-23 21:07:23 -0300

    add gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   css: ["primeflex/primeflex.css", "@/static/global.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["@/plugins/vue-awesome"],
+  plugins: ["@/plugins/vue-awesome", "@/plugins/gtag"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
